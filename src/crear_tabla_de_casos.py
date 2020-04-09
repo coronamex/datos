8fa1449261bcd1baf35980c07f1ab44d653d96a0
surh <sur00mx@gmail.com>
2020-04-09 09:30:42 -0700

    Script para crear tabla resp SINAVE

--- src/crear_tabla_de_casos.py
+++ src/crear_tabla_de_casos.py
@@ -159,4 +159,4 @@ if __name__ == "__main__":
                                            'procedencia', 'fecha_llegada'])
 
     # Escribir el archivo final
-    Tab.to_csv(args.tabla_csv, sep=",", index=False, na_rep = "NA")
+    Tab.to_csv(args.tabla_csv, sep=",", index=False, na_rep="NA")
