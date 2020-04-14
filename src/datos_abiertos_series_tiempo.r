c32896511fed5781bb3652c631b66699e0b18f7b
surh <sur00mx@gmail.com>
2020-04-14 09:01:56 -0700

    Reorganizando y creaond serie de tiempo nacional

--- src/datos_abiertos_series_tiempo.r
+++ src/datos_abiertos_series_tiempo.r
@@ -1,3 +1,4 @@
+#!/usr/bin/env Rscript
 library(tidyverse)
 
 args <- list(base_de_datos = "datos_abiertos/base_de_datos.csv",
@@ -29,7 +30,10 @@ Dat <- Dat %>%
 Serie_confirmados <- Dat %>%
   split(.$ENTIDAD_UM) %>%
   map_dfr(function(d){
-    d <- Dat %>% filter(ENTIDAD_UM == "01")
+    # d <- Dat %>% filter(ENTIDAD_UM == "01")
+    # d <- Dat %>% filter(ENTIDAD_UM == "06")
+    # d <- Dat %>% filter(ENTIDAD_UM == "25")
+    # cat(unique(d$ENTIDAD_UM), "\n")
     
     d <- d %>%
       filter(RESULTADO == "1")
@@ -39,27 +43,49 @@ Serie_confirmados <- Dat %>%
     fecha_defuncion <- table(d$FECHA_DEF)
     # names(fecha_sintomas)
     
-    bind_rows(tibble(fecha = names(fecha_sintomas) %>% parse_date(format = "%Y-%m-%d"),
-                     numero = fecha_sintomas,
-                     grupo = "sintomas_nuevos"),
-              tibble(fecha = names(fecha_ingreso) %>% parse_date(format = "%Y-%m-%d"),
-                     numero = fecha_ingreso,
-                     grupo = "ingreso_nuevos"),
-              tibble(fecha = names(fecha_defuncion) %>% parse_date(format = "%Y-%m-%d"),
-                     numero = fecha_defuncion,
-                     grupo = "muertes_nuevas")) %>%
+    res <-  bind_rows(tibble(fecha = names(fecha_sintomas),
+                       numero = fecha_sintomas,
+                       grupo = "sintomas_nuevos"),
+                tibble(fecha = names(fecha_ingreso),
+                       numero = fecha_ingreso,
+                       grupo = "ingreso_nuevos"))
+    if(length(fecha_defuncion)){
+      res <- res %>%
+        bind_rows(tibble(fecha = names(fecha_defuncion),
+                         numero = fecha_defuncion,
+                         grupo = "muertes_nuevas"))
+    }else{
+      res <- res %>%
+        bind_rows(tibble(fecha = sort(names(fecha_sintomas), decreasing = TRUE)[1],
+                         numero = 0,
+                         grupo = "muertes_nuevas"))
+    }
+    res %>%
+      mutate(fecha = fecha %>% parse_date(format = "%Y-%m-%d")) %>%
       pivot_wider(fecha, names_from = "grupo", values_from = "numero",
                   values_fill = list(numero = 0)) %>%
       arrange(fecha) %>%
       mutate(sintomas_acumulados = cumsum(sintomas_nuevos),
              ingreso_acumulados = cumsum(ingreso_nuevos),
              muertes_acumuladas = cumsum(muertes_nuevas))
+      # print(n = 100)
   }, .id = "ENTIDAD_UM")
 Serie_confirmados %>%
   mutate(estado = estados_lut[ENTIDAD_UM]) %>%
   select(-ENTIDAD_UM) %>%
+  # filter(fecha == "2020-04-13")
+  # select(estado)
+  # table
   write_csv(path = file.path(args$dir_salida, "serie_tiempo_estados_um_confirmados.csv"))
 
-
-
-
+Serie_confirmados %>%
+  select(-ENTIDAD_UM, -sintomas_acumulados, -ingreso_acumulados, -muertes_acumuladas) %>%
+  group_by(fecha) %>%
+  # arrange(fecha) 
+  summarise_all(sum) %>%
+  arrange(fecha) %>%
+  mutate(sintomas_acumulados = cumsum(sintomas_nuevos),
+         ingreso_acumulados = cumsum(ingreso_nuevos),
+         muertes_acumuladas = cumsum(muertes_nuevas)) %>%
+  # print(n = 100) %>%
+  write_csv(path = file.path(args$dir_salida, "serie_tiempo_nacional_confirmados.csv"))
