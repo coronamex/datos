c808f8ff1a948173848eb3905c3bfa15025a7d21
surh <sur00mx@gmail.com>
2020-04-17 18:04:05 -0700

    Actualización dge 2020-04-17

--- src/procesar_ssa_dge.sh
+++ src/procesar_ssa_dge.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 DIR="./ssa_dge/"
-fecha="2020-04-16"
+fecha="2020-04-17"
 
 echo $DIR
 echo $fecha
@@ -9,7 +9,7 @@ echo $fecha
 # touch $DIR/$fecha/test
 
 ./src/crear_tabla_de_casos.py --tabla_csv $DIR/$fecha/tabla_casos_confirmados.csv --pdf_dge $DIR/$fecha/Tabla_casos_positivos_COVID-19_resultado_InDRE_*.pdf
-./src/obtener_datos_de_mapa.py --dir_salida $DIR/$fecha --url https://ncov.sinave.gob.mx/mapa.aspx/Grafica22
+./src/obtener_datos_de_mapa.py --dir_salida $DIR/$fecha --url https://covid19.sinave.gob.mx/mapa.aspx/Grafica22
 ./src/procesar_tablas_principales.r --dir $DIR/$fecha
 
 rm $DIR/tabla_casos_confirmados.csv
