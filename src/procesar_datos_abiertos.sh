c55a205219c19b777de25239eae7e53baf2ce188
surh <sur00mx@gmail.com>
2020-04-26 17:57:57 -0700

    Actualización 2020-04-26

--- src/procesar_datos_abiertos.sh
+++ src/procesar_datos_abiertos.sh
@@ -9,12 +9,28 @@ echo $fecha
 
 echo "Borrando vínculo"
 rm $DIR/base_de_datos.csv
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit 1
+fi
 
 echo "Creando vínculo"
 ln -s $fecha/base_de_datos.csv $DIR/base_de_datos.csv
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit 1
+fi
 
 echo "Series de tiempo"
 Rscript src/datos_abiertos_series_tiempo.r
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit 1
+fi
 
 echo "Fechas confirmacion"
 Rscript src/datos_abiertos_series_tiempo_fecha_confirmacion.r
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit 1
+fi
